94d71e1656696e923d4123446692db3bb524e27f
halinhtvn3a <halinh150@gmail.com>
2024-05-29 18:59:26 +0700

    Deploy_test

---
+++ API/Program.cs
@@ -0,0 +1,138 @@
+
+//namespace API
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            // Add services to the container.
+
+//            builder.Services.AddControllers();
+//            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//            builder.Services.AddEndpointsApiExplorer();
+//            builder.Services.AddSwaggerGen();
+
+//            var app = builder.Build();
+
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseSwagger();
+//                app.UseSwaggerUI();
+//            }
+
+//            app.UseHttpsRedirection();
+
+//            app.UseAuthorization();
+
+
+//            app.MapControllers();
+
+//            app.Run();
+//        }
+//    }
+//}
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Repositories.Helper;
+using Services.Interface;
+using System.Text;
+using Services;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                  {
+                    new OpenApiSecurityScheme
+                    {
+                      Reference = new OpenApiReference
+                      {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                      },
+                      Scheme = "oauth2",
+                      Name = "Bearer",
+                      In = ParameterLocation.Header,
+
+                    },
+                    new List<string>()
+                  }
+                });
+            });
+
+            // Add JWT Authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+
+            //email
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddTransient<IMailService, MailService>();
+
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication(); // Ensure authentication middleware is added
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ DAOs/BranchDAO.cs
@@ -0,0 +1,95 @@
+﻿using DAOs;
+using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAOs
+{
+    public class BranchDAO
+    {
+        private readonly CourtCallerDbContext dbContext = null;
+
+        public BranchDAO()
+        {
+            if (dbContext == null)
+            {
+                dbContext = new CourtCallerDbContext();
+            }
+        }
+
+        // hàm getbranch trả danh sách branch, lấy luôn courtid (là khóa ngoại)
+        public List<Branch> GetBranches()
+        {
+            var branches = dbContext.Branches
+                .Include(b => b.Courts)
+                .ToList();
+
+            return   branches.Select(b => new Branch
+            {
+                BranchId = b.BranchId,
+                Address = b.Address,
+                Description = b.Description,
+                Picture = b.Picture,
+                OpenTime = b.OpenTime,
+                CloseTime = b.CloseTime,
+                OpenDay = b.OpenDay,
+                Status = b.Status,
+                Courts = b.Courts.Select(c => new Court
+                {
+                    CourtId = c.CourtId
+                    
+                }).ToList()
+            }).ToList();
+
+            
+        }
+
+
+
+
+        public Branch GetBranch(string id)
+        {
+            return dbContext.Branches.FirstOrDefault(m => m.BranchId.Equals(id));
+        }
+
+        public Branch AddBranch(Branch Branch)
+        {
+            dbContext.Branches.Add(Branch);
+            dbContext.SaveChanges();
+            return Branch;
+        }
+
+        public Branch UpdateBranch(string id, Branch Branch)
+        {
+            Branch oBranch = GetBranch(id);
+            if (oBranch != null)
+            {
+                oBranch.Address = Branch.Address;
+                oBranch.Description = Branch.Description;
+                oBranch.Picture = Branch.Picture;
+                oBranch.OpenTime = Branch.OpenTime;
+                oBranch.CloseTime = Branch.CloseTime;
+                oBranch.OpenDay = Branch.OpenDay;
+                dbContext.Update(oBranch);
+                dbContext.SaveChanges();
+            }
+            return oBranch;
+        }
+
+        public void DeleteBranch(string id)
+        {
+            Branch oBranch = GetBranch(id);
+            if (oBranch != null)
+            {
+                oBranch.Status = false;
+                dbContext.Update(oBranch);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
